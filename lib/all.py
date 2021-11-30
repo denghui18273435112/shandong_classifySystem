662ee2d3cb383df6a011d60757e090c78ec8f188
denghui <314983713@qq.com>
2021-11-30 11:11:20 +0800

    完成山东诚信的基本构建

--- lib/all.py
+++ lib/all.py
@@ -129,6 +129,7 @@ class all:
         print(self.new_url)
         print(self.header)
         print(self.data)
+        print("\n")
 
         body = requests.post(url=self.new_url,headers=self.header,json=self.data)
         inData = update_data(self.inData,self.data,self.new_url,self.header,body.json(),json.loads(self.inData["response_expect_result"]),self.conftest)
