6aec989b681cc3655efe1d32ffdb226e9128d6b3
denghui <314983713@qq.com>
2021-12-13 18:14:57 +0800

    重新构建

--- test_case/test_all.py
+++ test_case/test_all.py
@@ -7,7 +7,7 @@ from lib.all import all
 @allure.epic("山东分类系统")
 class Test_all(object):
 
-    @pytest.mark.parametrize("Data",ExcelData("test"))
+    @pytest.mark.parametrize("Data",ExcelData("test_GetRecordForStudy"))
     def test_ParameterlessAdjustment(self,token_1,token_2,token_3,company_id_1,company_id_2,company_id_3,Data,
                                      year):
         """所有测试用例集合"""
