c4de03496b72652d59cc48997f01e38cc3b392e2
denghui18273435112 <314983713@qq.com>
2022-01-24 15:42:04 +0800

    公司-02

---
+++ tools/test.py
@@ -0,0 +1,46 @@
+import requests
+import pytest
+from tools.ExcelData import ExcelData
+import allure
+from tools.caseCheck import caseCheck
+from lib.all import all
+
+data = {"type":"I","Name":"zxs-sd","Vcode":"denghui","Pwd":"21ad15b19ad2fcc3ed51c089a8cc39ca"}
+url = "http://60.216.62.188:10263/base/home/Login"
+header = {"Content-Type": "application/json"}
+body = requests.post(url=url,json=data,headers=header)
+token = "{0}={1}".format("cipmcms-token",body.cookies["cipmcms-token"])
+
+
+    #身份证查询个人信息 删除
+    # for x in ["431225199212061818","M30102575","362130197312312425","HO4983324"]:
+    #     url1 = "http://sc.maintain.giiatop.com/api/member/DeleteMember"
+    #     data1= {"idNumber":"{}".format(x)}
+    #     header1 = {"Cookie":token}
+    #     body1 = requests.post(url=url1,json=data1,headers=header1)
+    #     body1.json()
+    #     allure.attach(body=json.dumps(body1.json()), name="身份证查询个人信息查询返回的数据", attachment_type=allure.attachment_type.TEXT)
+
+#获取列表档案信息档案
+url2 = "http://60.216.62.188:10263/api/member/GetMemberTriningOffline"
+data2= {"year":2022,"className":"","pageNum":1,"pageSize":20,"total":275,"createdTime":[]}
+header2 = {"Cookie":token}
+body2 = requests.post(url=url2,json=data2,headers=header2)
+body2.json()
+
+
+#获取需要删除的数据id
+delete_id = []
+len_s= body2.json()["data"]["list"]
+for x  in range(len(len_s)):
+    if  len_s[x]["className"] == "自动审核数据":
+        delete_id.append(len_s[x]["id"])
+
+#删除档案数据
+for x  in delete_id:
+    url3 = "http://60.216.62.188:10263/api/home/TrainingClear"
+    data3= {"ids":[x]}
+    header3 = {"Cookie":token}
+    print(x)
+    body3 = requests.post(url=url3,json=data3,headers=header3)
+    body3.json()
